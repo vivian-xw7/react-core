f485da4b554234c15d624cc65522cf73a8875c8d
Vivian Hayes <vivian.xw7@gmail.com>
2024-05-01 16:25:52 -0500

    add tab button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import Header from './components/Header'
+import Header from './components/Header/Header.jsx';
 import { CORE_CONCEPTS } from './data';
-import CoreConcept from './components/CoreConcept';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx'
 
 function App() {
   return (
@@ -31,6 +32,12 @@ function App() {
               image={CORE_CONCEPTS[3].image}
             />
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+          </menu>
         </section>
       </main>
     </div>
