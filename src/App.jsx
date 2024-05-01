7803c761a11af917b3ca1289f93f0a0aec5c695b
Vivian Hayes <vivian.xw7@gmail.com>
2024-05-01 15:43:00 -0500

    extract header

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,4 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const words = reactDescriptions[genRandomInt(2)]
-
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {words} React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
+import Header from './components/Header'
 
 function App() {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
-function Header() {
-    return (
-        <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-            <h1>React Essentials</h1>
-            <p>
-            Fundamental React concepts you will need for almost any app you are
-            going to build!
-            </p>
-        </header>
-    )
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
 
-export default Header
+export default function Header() {
+  const words = reactDescriptions[genRandomInt(2)]
+
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {reactDescriptions} React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
