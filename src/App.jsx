866cc9886588952ef6489ce62750d0fbf6c5e642
Vivian Hayes <vivian.xw7@gmail.com>
2024-08-18 22:38:56 -0500

    make concise

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, Fragment } from 'react';
+import { useState } from 'react';
 
 import Header from './components/Header/Header.jsx';
 import { CORE_CONCEPTS } from './data';
@@ -28,7 +28,7 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <>
       <Header />
       <main>
         <section id='core-concepts'>
@@ -55,7 +55,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </Fragment>
+    </>
   );
 }
 
